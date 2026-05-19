785b040bfd7e2c0bd4068c83e9e54ea47aec13b7
EpifaniusDaeli <warnaungu155@gmail.com>
2026-05-20 06:51:59 +0700

    menambahkan bagian epi

---
+++ src/epifanius_auth/auth_system.py
@@ -0,0 +1,6 @@
+#register
+#login
+#verifikasi akun
+'''1.cek username
+2.cek password
+3.simpan akun ke file'''
---
+++ src/epifanius_auth/login_history.py
@@ -0,0 +1,5 @@
+#riwayat login user
+'''contoh: "Budi login pada 18 mei 2026
+1.file handling
+2.append mode
+3.logging sederhana"'''
